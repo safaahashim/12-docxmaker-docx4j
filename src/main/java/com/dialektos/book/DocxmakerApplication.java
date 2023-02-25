873d445cf4caef48497cf5bc66801a8ddbdb785e
safaa <safaa@berkeley.edu>
2023-02-25 15:47:30 -0800

    Initial commit

---
+++ src/main/java/com/dialektos/book/DocxmakerApplication.java
@@ -0,0 +1,13 @@
+package com.dialektos.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocxmakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocxmakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dialektos/book/DocxmakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dialektos.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocxmakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
